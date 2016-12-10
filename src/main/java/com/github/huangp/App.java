ce588b24edef132e9bc552b387d66808baef39a4
Patrick Huang <pahuang@redhat.com>
2016-12-10 11:43:38 +1000

    enable logging

--- src/main/java/com/github/huangp/App.java
+++ src/main/java/com/github/huangp/App.java
@@ -1,13 +1,12 @@
 package com.github.huangp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        log.info("Hello World!");
     }
 }
